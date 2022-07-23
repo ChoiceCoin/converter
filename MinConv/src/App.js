ff6aeaed2b18093f2603e1a07a5349bdec7053cc
B Haney <43055154+Bhaney44@users.noreply.github.com>
2022-07-22 21:05:04 -0700

    Update App.js

--- MinConv/src/App.js
+++ MinConv/src/App.js
@@ -5,14 +5,13 @@ import { PeraWalletConnect } from "@perawallet/connect";
 
 const perawallet = new PeraWalletConnect()
 
-
 function SmartButton() {
   const recieverAddress = () => {
     console.log("Transaction Sending.")
     const recieverValue = document.getElementById('reciever').value
-    document.getElementById('message0').textContent = "Sending to "+recieverValue;
+    document.getElementById('message0').textContent = "Sending to: "+recieverValue;
     const amountValue = document.getElementById('amount').value
-    document.getElementById('message1').textContent = "Amount to send"+amountValue;
+    document.getElementById('message1').textContent = "Amount to send: "+amountValue;
   }
   return (
     <button id='button' onClick={recieverAddress}>Convert</button>
@@ -24,14 +23,17 @@ async function walletConnect() {
   await perawallet.connect()
     console.log('Connect')
   }
-  
-
 
 // React functions must return a React component
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          <div>
+          <button id='button1' onClick={walletConnect}>Connect Wallet</button>
+          </div>
+        </p>
         <h1>
           Choice Coin Converter
         </h1>
@@ -45,20 +47,20 @@ function App() {
             <input id="amount" type="text"/>
           </div>
         </p>
-          <div>
-            <SmartButton />
-          </div>
+        <p>
+        <div>
+         You must agree to Choice Coin's Terms and Conditions.
+        </div>
+        <div>
+          Check the box to affirm acceptance.
+          <input id="TC" type="radio"/>
+        </div>
+      </p>
+        <div>
+          <SmartButton />
+        </div>
         <p id="message0"></p>
         <p id="message1"></p>
-        <p>
-          <div>
-            <button onClick={walletConnect}>Connect Wallet</button>
-          </div>
-          <div>
-            You agree to Choice Coin's Terms and Conditions
-          </div>
-        </p>
-
       </header>
     </div>
   );
