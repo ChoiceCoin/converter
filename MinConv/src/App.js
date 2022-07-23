562f42b9bd7bbf948e54c85abe172fe797ef579f
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-07-23 16:59:47 +0100

    adding perawallet transaction code 📲

--- MinConv/src/App.js
+++ MinConv/src/App.js
@@ -1,43 +1,89 @@
 // Imports
 import './App.css';
-//import algosdk from "algosdk";
+import algosdk from "algosdk";
 import { PeraWalletConnect } from "@perawallet/connect";
+import { useEffect } from 'react';
 
 const perawallet = new PeraWalletConnect()
 
-function SmartButton() {
-  const recieverAddress = () => {
-    console.log("Transaction Sending.")
-    const recieverValue = document.getElementById('reciever').value
-    document.getElementById('message0').textContent = "Sending to: "+recieverValue;
-    const amountValue = document.getElementById('amount').value
-    document.getElementById('message1').textContent = "Amount to send: "+amountValue;
-  }
-  return (
-    <button id='button' onClick={recieverAddress}>Convert</button>
-  )
+// algoClient
+const algod_token = {
+  "X-API-Key": "" //ADD YOUR API KEY
 }
+const algod_address = "https://testnet-algorand.api.purestake.io/ps2";
+const headers = "";
+
+const algodClient = new algosdk.Algodv2(algod_token, algod_address, headers);
+
+//get address
+const address = localStorage.getItem('address');
+
+// converter address
+const converterAddress = 'GJBPDHMNYDHAHLDCXXWLVHJDK3U3OYX5VHEUHREI5SXMLLBM6J4ALBZSWI'
+
+//asset id
+const ASSET_ID = 42771692;
+
+/// transaction code
+const  transaction = async () => {
+  const suggestedParams = await algodClient.getTransactionParams().do();
+  const enc = new TextEncoder();
+  const note = enc.encode('Transaction with perawallet');
+
+ const txn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+    from: address,
+    to: converterAddress,
+    amount: 1,
+    assetIndex: ASSET_ID,
+    note : note,
+    suggestedParams,
+  });
+  const optInTxn = [{txn : txn, signers: [address]}]
+  const signedTxn = await perawallet.signTransaction([optInTxn])
+    await algodClient.sendRawTransaction(signedTxn).do();
+}
+
+
+
 
 // Wallet Connect
 async function walletConnect() {
-  await perawallet.connect()
+  const newAccounts= await perawallet.connect()
+  localStorage.setItem("address", newAccounts[0]);
+  window.location.reload()
     console.log('Connect')
   }
+// wallet disconnect
 
+const disconnect = () => {
+  perawallet.disconnect()
+  localStorage.removeItem("address");
+  window.location.reload()
+}
 // React functions must return a React component
 function App() {
+// useEffect to connect to perawallet after reload
+  useEffect(() => {
+    perawallet.reconnectSession().then((accounts) => {
+      if (accounts.length) {
+            localStorage.setItem("address", accounts[0]);
+      }
+      perawallet.connector?.on("disconnect", () => {
+        localStorage.removeItem("address");
+      });
+    })
+    .catch((e) => console.log(e));
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
           <div>
-          <button id='button1' onClick={walletConnect}>Connect Wallet</button>
+          <button id='button1' onClick={address ? disconnect:  walletConnect}>{address ? 'Disconnect' : 'Connect Wallet'}</button>
           </div>
-        </p>
         <h1>
           Choice Coin Converter
         </h1>
-        <p>
           <div>
             Ethereum Address:
             <input id="reciever" type="text"/>
@@ -46,7 +92,6 @@ function App() {
             Amount:
             <input id="amount" type="text"/>
           </div>
-        </p>
         <p>
         <div>
          You must agree to Choice Coin's Terms and Conditions.
@@ -57,7 +102,7 @@ function App() {
         </div>
       </p>
         <div>
-          <SmartButton />
+        <button id='button' onClick={transaction}>Convert</button>
         </div>
         <p id="message0"></p>
         <p id="message1"></p>
